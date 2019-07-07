e77513d2f5ab02b1ad3f0fda3304e18750d84f8d
jamiebdavis <jamied1985@gmail.com>
2019-07-07 15:15:00 +0100

    rendered first component

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-/**
- * @format
- */
+/* eslint-disable prettier/prettier */
+//Import a library to help create component.
+import React from 'react';
+import { Text, AppRegistry } from 'react-native';
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+//Create a component.
+const App = () => {
+    return <Text>Some Text</Text>;
+};
 
-AppRegistry.registerComponent(appName, () => App);
+//Render it to the device.
+AppRegistry.registerComponent('albums', () => App);
