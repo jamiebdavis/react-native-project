8e1bece94d64f2ec827f7aed62f467b756a61cea
jamiebdavis <jamied1985@gmail.com>
2019-07-09 22:34:23 +0100

    made header re usable

--- src/components/header.js
+++ src/components/header.js
@@ -3,19 +3,28 @@ import React from "react";
 import { Text, View } from "react-native";
 
 //Make a components.
-const Header = () => {
+const Header = props => {
   const { textStyle, viewStyle } = styles;
 
   return (
     <View style={viewStyle}>
-      <Text style={textStyle}>Albums!</Text>
+      <Text style={textStyle}>{props.headerText}</Text>
     </View>
   );
 };
 
 const styles = {
   viewStyle: {
-    backgroundColor: "f8f8f8"
+    backgroundColor: "skyblue",
+    justifyContent: "center",
+    alignItems: "center",
+    height: 60,
+    paddingTop: 25,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 8 },
+    shadowOpacity: 0.8,
+    elevation: 2,
+    position: "relative"
   },
   textStyle: {
     fontSize: 20
