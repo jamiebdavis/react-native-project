83c43ff459ee7f8a8487ab9584ae416d41b002cb
jamiebdavis <jamied1985@gmail.com>
2019-07-14 10:08:32 +0100

    changed to class component

---
+++ src/components/album-list.js
@@ -0,0 +1,14 @@
+import React, { Component } from "React";
+import { View, Text } from "react-native";
+
+class AlbumList extends Component {
+  render() {
+    return (
+      <View>
+        <Text>'Album List!!!'</Text>
+      </View>
+    );
+  }
+}
+
+export default AlbumList;
